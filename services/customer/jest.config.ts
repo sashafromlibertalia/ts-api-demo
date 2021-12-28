6fb5b5930ab6dea7f6bdadebc02c6cf738a93502
Alexander <sashafromlibertalia@gmail.com>
2021-12-28 23:55:42 +0300

    chore: test fix rootdir

--- services/customer/jest.config.ts
+++ services/customer/jest.config.ts
@@ -1,12 +1,13 @@
 module.exports = {
-    "roots": ["test"],
-    "testMatch": [
+    roots: ["test"],
+    rootDir: "../",
+    testMatch: [
         "**/tests/**/*.+(ts|tsx|js)",
         "**/?(*.)+(spec|test).+(ts|tsx|js)"
     ],
-    "transform": {
+    transform: {
         "^.+\\.(ts|tsx)$": "ts-jest"
     },
-    "collectCoverage": true,
-    "verbose": true
+    collectCoverage: true,
+    verbose: true
 }
